4749cef7f9b1d70e5fdbe8e07f60718b53f9a34b
CtrlArnav <royarnav708@gmail.com>
2026-06-22 15:23:12 +0530

    Connect MongoDB Atlas database

--- server/src/index.js
+++ server/src/index.js
@@ -1,10 +1,12 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import mongoose from "mongoose";
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
+const MONGO_URI=process.env.MONGO_URI;
 
 app.use(cors());
 app.use(express.json());
@@ -13,6 +15,12 @@ app.get("/api/health", (req, res) => {
     res.json({ status: "ok", message: "Server is running" });
 });
 
-app.listen(PORT, () => {
-    console.log("Server running on http");
+mongoose.connect(MONGO_URI).then(() => {
+    console.log("Connected to MongoDB");
+    app.listen(PORT, () => {
+        console.log("Server Running on http");
+    });
+}).catch((err) => {
+    console.error(err);
+    process.exit(1);
 });
